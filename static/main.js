40e076091de9786a20fe23cdb7a4d36a97fd655c
Rimu Eirnarn <Rimuru720@proton.me>
2023-08-02 20:16:16 +0700

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 // lmao, pythonic
-var __version__ = "0.0.1"
+var __version__ = "0.0.2"
 
 /**
  * show current project version or atleast the new version/rework version.
@@ -9,7 +9,31 @@ function show_version() {
 }
 
 function __redir_anime() {
-    document.location = "/_anime.html"
+    // do i look like care about this?
+    var target = `target-${Math.round(Math.random() * 100)}`
+    var data = `<div class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Anime Redirection</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Proceed?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+        <button type="button" class="btn btn-primary" id="${target}">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>`
+    var body = document.querySelector("body")
+    body.innerHTML += data
+    var target_element = document.querySelector(target)
+    target_element.onclick = () => {
+        document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    }
 }
 
 /**
@@ -25,3 +49,7 @@ async function __beta_popup() {
 }
 
 // To Be Added
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  console.info(`Loaded ${window.location.host}/main.js [${__version__}]`)
+});
