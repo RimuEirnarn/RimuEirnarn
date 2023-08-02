3d1079e83610ef44ca46bf32ef7ba068a37a5235
RimuEirnarn <Rimuru720@Proton.me>
2023-08-02 21:20:43 +0700

    Re-style navbar and few stuff

--- static/main.js
+++ static/main.js
@@ -8,32 +8,55 @@ function show_version() {
     console.log(__version__)
 }
 
+function refresh_modal(){
+    var container = document.querySelector(".container-0")
+    var x = document.querySelector(".container-0 > .modal")
+    if (x) {
+        x.classList.add("fade")
+        x.style = ""
+        sleep(0.3).then(() => {})
+    }
+    while (container.firstChild) {
+          container.removeChild(container.firstChild);
+    }
+}
+
 function __redir_anime() {
     // do i look like care about this?
     var target = `target-${Math.round(Math.random() * 100)}`
-    var data = `<div class="modal" tabindex="-1">
+    var container = document.querySelector(".container-0")
+    var data = `<div class="modal fade" tabindex="-1" id="modal-${target}">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content bg-dark">
       <div class="modal-header">
         <h5 class="modal-title">Anime Redirection</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="refresh_modal()"></button>
       </div>
       <div class="modal-body">
         <p>Proceed?</p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="refresh_modal()">No</button>
         <button type="button" class="btn btn-primary" id="${target}">Yes</button>
       </div>
     </div>
   </div>
 </div>`
-    var body = document.querySelector("body")
-    body.innerHTML += data
-    var target_element = document.querySelector(target)
+    var fragment = document.createDocumentFragment()
+    var inside_container = document.createElement('div')
+    inside_container.innerHTML = data
+    fragment.appendChild(inside_container)
+    container.appendChild(fragment)
+    var target_element = document.querySelector(`#${target}`)
     target_element.onclick = () => {
         document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     }
+    sleep(0.1).then(() => {})
+
+    var modal = new bootstrap.Modal(document.querySelector(`#modal-${target}`), {
+        focus: true
+    })
+    modal.show()
 }
 
 /**
